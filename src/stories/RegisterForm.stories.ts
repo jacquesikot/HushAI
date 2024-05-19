1f505d60b8807d390788731a62e3b9ebb858c715
Jacques Ikot <jacquesikot@gmail.com>
2024-05-19 06:23:33 +0100

    fix RegisterForm.stories for vercel

--- src/stories/RegisterForm.stories.ts
+++ src/stories/RegisterForm.stories.ts
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import RegisterForm from '@/components/auth/RegisterForm';
+import { fn } from '@storybook/test';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
@@ -14,7 +15,7 @@ const meta = {
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {},
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: {},
+  args: { handleClickLogin: fn() },
 } satisfies Meta<typeof RegisterForm>;
 
 export default meta;
@@ -22,5 +23,7 @@ type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Normal: Story = {
-  args: {},
+  args: {
+    handleClickLogin: fn(),
+  },
 };
